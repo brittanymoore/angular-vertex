aacfda8521935506145b0db2ebd3962547bf2a18
brittanymoore@gm.com <moore.brittanyann@gmail.com>
2016-11-29 10:02:14 -0500

    Added unit tests for todo component.

--- src/app/todo/todo.spec.ts
+++ src/app/todo/todo.spec.ts
@@ -1,5 +1,34 @@
-describe('TODO - Sanity Check', () => {
-    it('always passes', () => {
-        expect(1).toBe(1);
+
+import { ToDoService } from './todo.service';
+import { Common } from './../app.common.service';
+import { MockModule } from './../mock/mock.module';
+
+import { HttpModule } from '@angular/http';
+import { TestBed, inject, async, getTestBed } from '@angular/core/testing';
+
+describe('todo service', () => {
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpModule,
+                MockModule
+            ],
+            providers: [
+                ToDoService,
+                Common
+            ]
+        });
+        TestBed.compileComponents();
     });
+
+    it('sanity check', async(() => {
+        let todoService: ToDoService = getTestBed().get(ToDoService);
+        todoService.getTasks().subscribe(
+            (tasks) => {
+                expect(tasks.length).toBe(2);
+            }
+        )
+    }))
+
 });
