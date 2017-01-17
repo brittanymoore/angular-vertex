2d1414e406347e9093002240635b94e1356d5376
brittanymoore@gm.com <moore.brittanyann@gmail.com>
2017-01-17 09:32:44 -0500

    Updated styles for IE.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {ViewEncapsulation} from '@angular/core';
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None // apply styles globally
 })
 
 export class AppComponent {
